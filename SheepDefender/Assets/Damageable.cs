24f337a6a38a30300ecd0ff9cbcc9807d36677bb
Kai Kulju <tronza@gmail.com>
2013-10-30 16:30:36 +0200

    Added turret that shoots at a hardcoded target

---
+++ SheepDefender/Assets/Damageable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damageable : MonoBehaviour
+{
+	public float health = 100f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!this.HasHealth ()) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void ReceiveDamage (float damage)
+	{
+		// TODO: do something fancy with the amount of received damage ;-)
+		// if (this.hasArmour ()) {
+		// 	 this.health -= damage / 2;
+		// } etc.
+
+		this.health -= damage;
+	}
+
+	public bool HasHealth ()
+	{
+		return this.health >= 0;
+	}
+}
---
+++ SheepDefender/Assets/Turret.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour
+{
+	public float damage = 10f;
+	public float cooldownTime = 2f; // Seconds
+	public Damageable target = null; // TODO: change to private when not debugging!
+	
+	float cooldownRemaining = 0f;
+	
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		// TODO: it takes one extra frame (!) to find a new target
+		if (cooldownRemaining <= 0) {
+			if (this.HasTargetAcquired ()) {
+				DealDamage ();
+				cooldownRemaining = cooldownTime;
+			} else {
+				this.AcquireNextTarget ();
+			}
+		}
+		
+		cooldownRemaining -= Time.deltaTime;
+	}
+	
+	void DealDamage ()
+	{
+		if (this.target.HasHealth ()) {
+			this.target.ReceiveDamage (damage);
+			
+			if (this.target.HasHealth ()) {
+				this.AcquireNextTarget ();	
+			}
+			
+		} else {
+			this.target = null;
+			
+			this.AcquireNextTarget ();
+		}
+	}
+	
+	bool HasTargetAcquired ()
+	{
+		if (this.target != null) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	void AcquireNextTarget ()
+	{
+		this.target = this.FindTarget ();
+	}
+	
+	// TODO: implement something that checks for the nearest and most dangerous target
+	Damageable FindTarget ()
+	{
+		return this.target;
+	}
+}
