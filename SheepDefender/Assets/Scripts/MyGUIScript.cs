d793f650bf5a580817347a87361e9ad1131eab18
Serpenthelm <spam.dump.one@gmail.com>
2013-11-05 17:15:38 +0200

    Interface hide/show and turret placing
    Interface hide/show is working now.
    Added the creation of a turret, working on placing.
    Grid is displayed.

--- SheepDefender/Assets/Scripts/MyGUIScript.cs
+++ SheepDefender/Assets/Scripts/MyGUIScript.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class MyGUIScript : MonoBehaviour
 {
+	//public vars
 	public GameInfo myGameInfo;
 	public Texture2D turretImage;
-	bool hideToggleValue = false;
+	public TurretPlacer myTurretPlacer;
+		
+	//private vars
+	bool buttonsHidden = false;
+	bool allHidden = false;
 	GUIContent[] turrets;
-//	public Texture[] turretImages;
-//	int selectedTurret = -1;
+	int selectedTurret = -1;
+	bool selectedTurretChanged = false;
 	
 	//TODO: find a way to scale button icon, without stretching it
-//	GUILayoutOption[] hideButtonOptions = {GUILayout.ExpandHeight(true), GUILayout.MaxHeight (Screen.height)};
 	GUILayoutOption[] shopOptions = {GUILayout.MaxWidth (0.2f * Screen.width)};
 	
 	void Start()
@@ -25,55 +29,73 @@ public class MyGUIScript : MonoBehaviour
 	
 	void OnGUI ()
 	{
-		//use all the screen, dynamically adjust
-		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
-		
-		//stack the different stuff
-		GUILayout.BeginVertical ();
-		
-		//display player/level info
-		GUILayout.BeginHorizontal ();
-		//TODO: ask WTH player info they want to have
-		//TODO: add some padding on the sides
-		GUILayout.Label("Coins: "+myGameInfo.coins);
-		GUILayout.FlexibleSpace (); //fill
-		GUILayout.Label("Level: "+myGameInfo.level);
-		GUILayout.FlexibleSpace (); //fill
-		GUILayout.Label("Player name: "+myGameInfo.playerName);
-		GUILayout.EndHorizontal ();
-		
-		//display turret shop and close button
-		GUILayout.BeginVertical (shopOptions);
-		GUILayout.Label("Shop");
-		//TODO: handle clicks on toggle
-		GUILayout.Toggle(hideToggleValue, "hide");
-		
-		//Buttons cannot be grayed out in a SelectionGrid -> discard
-//		selectedTurret= GUILayout.SelectionGrid(selectedTurret, turretImages, 1);	
-
-//		if(GUILayout.Button(new GUIContent("Turret 1", turretImage))) {
-//			Debug.Log("turret 1");
-//		}
-//		if(GUILayout.Button(new GUIContent("Turret 2", turretImage))) {
-//			Debug.Log("turret 2");
-//		}
-//		if(GUILayout.Button(new GUIContent("Turret 3", turretImage))) {
-//			Debug.Log("turret 3");
-//		}
-		
-		if(GUILayout.Button(turrets[0])) {
-			Debug.Log("turret 0");
+		if (!allHidden) {
+			//use all the screen, dynamically adjust
+			GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+			
+			//stack the different stuff
+			GUILayout.BeginVertical ();
+			
+			//display player/level info
+			GUILayout.BeginHorizontal ();
+			//TODO: ask WTH player info they want to have
+			//TODO: add some padding on the sides
+			GUILayout.Label("Coins: "+myGameInfo.coins);
+			GUILayout.FlexibleSpace (); //fill
+			GUILayout.Label("Level: "+myGameInfo.level);
+			GUILayout.FlexibleSpace (); //fill
+			GUILayout.Label("Player name: "+myGameInfo.playerName);
+			GUILayout.EndHorizontal ();
+			
+			//display turret shop and close button
+			GUILayout.BeginVertical (shopOptions);
+			GUILayout.Label("Shop");
+			//TODO: handle clicks on toggle
+			buttonsHidden = GUILayout.Toggle(buttonsHidden, "hide");
+			
+			//NOTE: buttons cannot be grayed out in a SelectionGrid
+			//-> do not use a SelectionGrid, use buttons in a layout
+			
+			if (!buttonsHidden) {
+				if(GUILayout.Button(turrets[0])) {
+					selectedTurretChanged = true;
+					selectedTurret = 0;
+				}
+				if(GUILayout.Button(turrets[1])) {
+					selectedTurretChanged = true;
+					selectedTurret = 1;
+				}
+				if(GUILayout.Button(turrets[2])) {
+					selectedTurretChanged = true;
+					selectedTurret = 2;
+				}
+			}
+			
+			if (selectedTurretChanged) {
+				Debug.Log("Selected turret: " + selectedTurret);
+				selectedTurretChanged = false;
+				myTurretPlacer.createTurret(selectedTurret);
+			}
+			
+			GUILayout.FlexibleSpace (); //fill
+			GUILayout.EndVertical ();
+			GUILayout.EndVertical ();
+			GUILayout.EndArea ();
 		}
-		if(GUILayout.Button(turrets[1])) {
-			Debug.Log("turret 1");
-		}
-		if(GUILayout.Button(turrets[2])) {
-			Debug.Log("turret 2");
-		}
-		
-		GUILayout.FlexibleSpace (); //fill
-		GUILayout.EndVertical ();
-		GUILayout.EndVertical ();
-		GUILayout.EndArea ();
+	}
+	
+	bool isAllHidden()
+	{
+		return allHidden;
+	}
+	
+	void setAllHidden(bool hidden) {
+		this.allHidden = hidden;
+	}
+	
+	//-1 means none
+	int GetSelectedTurret()
+	{
+		return selectedTurret;
 	}
 }
