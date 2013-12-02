c47fd72b472f85d7816709f6fa51fec26bd15939
Serpenthelm <spam.dump.one@gmail.com>
2013-12-02 16:05:41 +0200

    Fixed rotation speed
    Rotation speed in mouse look now respects max degrees per second
    constraint.

--- SheepDefender/Assets/Scripts/Sheep.cs
+++ SheepDefender/Assets/Scripts/Sheep.cs
@@ -3,17 +3,13 @@ using System.Collections;
 
 public class Sheep : MonoBehaviour
 {
-	
-	//TODO: should be an array of weapons
-	
-	int currentWeapon = 1; //represents the weapon to use 1->red lazer, 2->green lazer
-	
 	public float moveSpeed = 10f; //in meters per second
-	public float rotationSpeed = 100.0F; //in degrees per sec
+	public float rotationSpeed = 180.0F; //in degrees per sec
 	public float jumpSpeed = 20.0F;
 	public float jumpUpTime = 0.5f; //time a jump goes up in secs
 	public int maxJumps = 2; //double jump allowed!
 	public bool relativeMove = true;
+	public Object[] weaponPrefabs;
 	
 	CharacterController cont;
 	Vector3 moveDir;
@@ -22,13 +18,15 @@ public class Sheep : MonoBehaviour
 	float jumpTimeLeft;
 	int jumpCount; //how many jumps have been done since touching the ground
 	float gravity;
-	Plane plane = new Plane(Vector3.up, 0);
+	Plane plane = new Plane(Vector3.up, 0F);
+	int currentWeapon;
 	
 	void Start () {
 		cont = GetComponent<CharacterController>();
 		jumpTimeLeft = 0F;
 		jumpCount = 0;
 		gravity = Physics.gravity.magnitude;
+		currentWeapon = 0;
 	}
 	
 	void Update () {
@@ -39,10 +37,10 @@ public class Sheep : MonoBehaviour
 	        rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 			
 			//if goind reverse, reverse rotation
-			if(advance < 0) {
+			if(advance < 0F) {
 				rotation = -rotation;
 			}
-			transform.Rotate(0, rotation, 0);
+			transform.Rotate(0F, rotation, 0F);
 			
 			//convert local forward to world forward and advance
 	        moveDir = transform.TransformDirection(Vector3.forward);
@@ -64,8 +62,12 @@ public class Sheep : MonoBehaviour
 				//create the rotation we need to be in to look at the target
 				Quaternion lookRotation = Quaternion.LookRotation(direction);
 				
+				float angle = Quaternion.Angle(transform.rotation, lookRotation);
+				float timeToComplete = angle / rotationSpeed;
+				
 				//rotate towards a direction, but not immediately (rotate a little every frame)
-				transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+				//The 3rd parameter is a number between 0 and 1, where 0 is the start rotation and 1 is the end rotation
+				transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime / timeToComplete);
 			}
 		}
 		//if you are standing on the ground
@@ -77,6 +79,7 @@ public class Sheep : MonoBehaviour
 		}
 		
 		//if the jump button was just pressed
+		//TODO: calculate real gravity acceleration
 		if(Input.GetButtonDown("Jump")) {
 			if(jumpCount < maxJumps) {
 				jumpCount += 1;
@@ -92,46 +95,15 @@ public class Sheep : MonoBehaviour
 		//takes absolute deltas, gravity must be applied by hand
 		cont.Move(moveDir);
 		
-//			if(cont.isGrounded) {
-//				moveDir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-//				jumpCount = 0;
-//				
-//				if(Input.GetButtonDown("Jump") && jumpCount <= maxJumps) {
-//					jumpCount+= 1;
-//					moveDir.y += jumpHeight;
-//				}
-//				
-//				moveDir *= moveSpeed;
-//			}
-//			else if(transform.position.y > -1.0f) {
-//				moveDir.y -= gravity;
-//			}
-//			
-//			cont.Move(moveDir * Time.deltaTime);
-//			
-//			//cast ray from camera to ground, get intersection point with ground layer and move light there
-//			float dist;
-//			Ray ray = Camera.mainCamera.ScreenPointToRay(Input.mousePosition);
-//			if (plane.Raycast(ray, out dist)) {
-//				Vector3 point = ray.GetPoint(dist);
-//				
-//				//find the vector pointing from our position to the target
-//				Vector3 _direction = (point - transform.position).normalized;
-//				 
-//				//create the rotation we need to be in to look at the target
-//				Quaternion _lookRotation = Quaternion.LookRotation(_direction);
-//				 
-//				//rotate us over time according to speed until we are in the required rotation
-//				transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
-//			}
-//			}
-		
-		/*changing weapon*/
-		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
-			currentWeapon++;
-			if (currentWeapon > 2) {
-				currentWeapon = 1;
+		//weapon switching
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		if (scroll != 0F){
+			if (scroll > 0F) {
+				++currentWeapon;
+			} else {
+				--currentWeapon;
 			}
+			currentWeapon %= weaponPrefabs.Length;
 		}
 	}
 	
