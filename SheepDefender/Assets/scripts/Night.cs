56e20078447228a041142d11a602b4e27b6157c3
Kai Kulju <tronza@gmail.com>
2013-12-04 15:30:39 +0200

    Changed light names

--- SheepDefender/Assets/scripts/Night.cs
+++ SheepDefender/Assets/scripts/Night.cs
@@ -6,8 +6,8 @@ public class Night : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//turn off the lights
-		GameObject light = GameObject.Find("Spotlight");
-		GameObject dLight = GameObject.Find("Directional light");
+		GameObject light = GameObject.Find("Top Dir Light");
+		GameObject dLight = GameObject.Find("Obq Dir Light");
 		light.GetComponent<Light>().intensity = 0f;
 		dLight.GetComponent<Light>().intensity = 0f;
 	}
